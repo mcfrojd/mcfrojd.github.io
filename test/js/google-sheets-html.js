de205ae3497342c0c7127fbd5cb3c58c44568fc5
mcfrojd <mcfrojd@gmail.com>
2020-02-14 21:15:44 +0100

    Update google-sheets-html.js

--- test/js/google-sheets-html.js
+++ test/js/google-sheets-html.js
@@ -15,7 +15,7 @@ google.load('visualization', '1', {
 var visualization;
 
 function drawVisualization() {
-    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1y8QRGUT0bb0Wx6lAHbMjNxU7Zs96WDJE9SXRiuQQfJc&output=html&usp=sharing');
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1oe8WKRm92M5r1C5AuDedT1Grt7eG4cC6D8rMjg7Bmz4&output=html&usp=sharing');
     query.setQuery('SELECT A, B, C, D label A "Duration", B "Song", C "Requested By", D "URL"');
     query.send(handleQueryResponse);
 }
